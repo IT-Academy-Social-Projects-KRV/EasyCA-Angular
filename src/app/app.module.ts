804808ce623122caf240fbed57d39773122da56e
swem-st <andre.steztt@gmail.com>
2021-08-17 10:27:51 +0300

    Created layout and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {DemoNgZorroAntdModule} from './ant-design-module/ng-zorro-antd-module';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginFormComponent,
+    AppComponent,
+    RegistrationFormComponent,
+    HomePageComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     DemoNgZorroAntdModule
   ],
-  providers: [],
+  providers: [ { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
