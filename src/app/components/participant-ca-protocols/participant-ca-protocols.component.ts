09b72c8cca458ac95789809443ffb2e4a95e3111
SashaKorniichuk <SashaKorniichuk2@gmail.com>
2021-11-10 14:56:33 +0200

    View list of car accidents

---
+++ src/app/components/participant-ca-protocols/participant-ca-protocols.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CarAccident } from 'src/app/models/carAccident';
+import { AccountService } from 'src/app/services/account.service';
+import { CAService } from 'src/app/services/ca.service';
+
+@Component({
+  selector: 'app-participant-ca-protocols',
+  templateUrl: './participant-ca-protocols.component.html',
+  styleUrls: ['./participant-ca-protocols.component.css']
+})
+export class ParticipantCAProtocolsComponent implements OnInit {
+
+  public accidentList: CarAccident[];
+  public isAccidentListEmpty = true;
+
+  constructor(private accountService: AccountService, private caService: CAService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.getDriverId();
+  }
+
+  getDriverId() {
+
+    this.accountService.getPersonalData().
+      subscribe((data: any) => {
+        console.log(data);
+        this.getCaProtocols(data.personalData.userDriverLicense.licenseSerialNumber);
+      },
+        err => {
+          this.toastr.warning("Please create personal data", err);
+        }
+      )
+  }
+
+  getCaProtocols(driverId: string) {
+    this.caService.getAllCAByDriverLicenseId(driverId)
+      .subscribe((data: any) => {
+        this.accidentList = data;
+
+        if (this.accidentList.length > 0) {
+          this.isAccidentListEmpty = false;
+          console.log(this.accidentList);
+        }
+        else {
+          this.toastr.warning("There are no CA protocols registered for this driver", "Warning");
+          this.isAccidentListEmpty = true;
+        }
+      },
+        err => {
+          this.toastr.error(err, "Error");
+        })
+  }
+}
