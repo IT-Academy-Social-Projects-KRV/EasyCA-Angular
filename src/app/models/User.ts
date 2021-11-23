5353eaa7a080dd2df9a21d3d6a3f8bb8b3afe227
pinkevycdima@gmail.com <pinkevycdima@gmail.com>
2021-11-23 15:31:02 +0200

    #185 create test for violation-list

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,9 +1,11 @@
-export interface User {
-    id: string,
-    email: string,        
-    firstName: string,
-    lastName: string,
-    password: string,     
-    confirmPassword: string,
-    clientURI:string
+export class User {
+    constructor(
+        public id: string,
+        public email: string,        
+        public firstName: string,
+        public lastName: string,
+        public password: string,     
+        public confirmPassword: string,
+        public clientURI:string
+    ) { }
 }
